70d9ae7ffd28914309d45611a228170390303316
Zain Baig <zain.ul.abidyn@gmail.com>
2019-07-22 22:30:13 +0100

    Updated stories service to use local data service for get requests instead of http client.

--- client/src/app/stories/stories.service.ts
+++ client/src/app/stories/stories.service.ts
@@ -2,26 +2,27 @@ import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Story} from "../models/story.model";
+import {DataService} from "../services/data.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class StoriesService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient,
+              private dataService: DataService) {
+  }
 
-    getStory(id: string): Observable<Story> {
-        return this.http.get<Story>(`api/stories/${id}`);
+  getStory(id: string) {
+    return this.dataService.getStory(id);
 
-    }
+  }
 
-    getStories(): Observable<Story[]> {
-        return this.http.get<Story[]>('api/stories');
+  getStories(): Observable<Story[]> {
+    return this.dataService.getStories();
+  }
 
-    }
-
-    createStory(story: FormData) {
-        return this.http.post('api/stories/create', story);
-    }
+  createStory(story: FormData) {
+    return this.http.post('api/stories/create', story);
+  }
 }

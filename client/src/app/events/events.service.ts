e8ba3838464fbef9dd15364cf7b904b55653ba87
Zain Baig <zain.ul.abidyn@gmail.com>
2019-07-22 22:30:51 +0100

    Updated events service to use local data service for get requests instead of http client.

--- client/src/app/events/events.service.ts
+++ client/src/app/events/events.service.ts
@@ -2,26 +2,27 @@ import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Event} from '../models/event.model';
+import {DataService} from "../services/data.service";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class EventsService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient,
+              private dataService: DataService) {
+  }
 
-    getEvent(id: string): Observable<Event> {
-        return this.http.get<Event>(`api/events/${id}`);
+  getEvent(id: string) {
+    return this.dataService.getEvent(id);
 
-    }
+  }
 
-    getEvents(): Observable<Event[]> {
-        return this.http.get<Event[]>('api/events');
+  getEvents(): Observable<Event[]> {
+    return this.dataService.getEvents();
+  }
 
-    }
-
-    createEvent(event: FormData) {
-        return this.http.post('api/events/create', event);
-    }
+  createEvent(event: FormData) {
+    return this.http.post('api/events/create', event);
+  }
 }

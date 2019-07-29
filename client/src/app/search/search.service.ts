4dc951f59741828dc057ba37fea86e6bc2770bb7
Zain Baig <mzbaig1@sheffield.ac.uk>
2019-07-29 21:05:33 +0100

    Added full-text offline search with multiple fields and date range filtering.

--- client/src/app/search/search.service.ts
+++ client/src/app/search/search.service.ts
@@ -1,25 +1,37 @@
 import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
+import {from, Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Event} from '../models/event.model';
 import {HttpParams} from '@angular/common/http'
 import {Search} from "../models/search.model";
+import {DataService} from "../services/data.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor(private http: HttpClient) {
+  serviceName = '[Search Service] :: ';
+
+  constructor(private http: HttpClient,
+              private dataService: DataService) {
   }
 
-  searchEvents(searchData: Search): Observable<Event[]> {
+  searchEvents(searchData: Search, online: boolean): Observable<Event[]> {
     let params = new HttpParams();
 
     for(let key in searchData){
       params = params.set(key, searchData[key])
     }
 
-    return this.http.get<Event[]>(`api/search/events?${params.toString()}`);
+    if (online) {
+      console.log(this.serviceName + "Searching " + params.toString() + " online.");
+      return this.http.get<Event[]>(`api/search/events?${params.toString()}`);
+    }
+
+    // The data service returns a promise from IndexedDB, turn it into an observable using RxJS 'from' to stay
+    // consistent with the type signature of this method.
+    console.log(this.serviceName + "Searching offline [" + JSON.stringify(searchData) + "]");
+    return from(this.dataService.searchEvents(searchData));
   }
 }
